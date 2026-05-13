7f85eefd21fa06b803917e0162df6301a6bfa321
Dinesh9418 <dineshsjadhav9418@gmail.com>
2026-05-14 00:35:06 +0530

    added modal

--- src/components/mainPage/MainPage.jsx
+++ src/components/mainPage/MainPage.jsx
@@ -1,45 +1,104 @@
-import React from "react";
-import Card from "./../card/Card";
+import React, { useState } from "react";
+
 import "./MainPage.css";
+import { Button, Modal } from "react-bootstrap";
 
-const MainPage = () => (
-  <div>
-    <div class="main">
-      {/* <!-- CENTER --> */}
+const MainPage = () => {
+  const [show, setShow] = useState(false);
 
-      <div class="feed-section">
-        <button class="feed-btn">EXPLORE FEED</button>
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <div>
+      <div className="main">
+        {/* <!-- CENTER --> */}
 
-        <div class="profile-card">
-          <div class="image-box">
-            <img
-              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1200&auto=format&fit=crop"
-              alt="girl"
-            />
+        <div className="feed-section">
+          <button className="feed-btn">EXPLORE FEED</button>
 
-            <div class="overlay"></div>
+          <div className="profile-card">
+            <div className="image-box">
+              <img
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1200&auto=format&fit=crop"
+                alt="girl"
+              />
 
-            <div class="profile-info">
-              <h2>Zoe Miller, 22</h2>
-              <button class="match-btn">+ Mate</button>
-              <p>4 Mutual Mates</p>
-              <div class="tags">
-                <div class="tag">Bollywood Nights</div>
-                <div class="tag">Chill Crowd</div>
-                <div class="tag">Party Regular</div>
+              <div className="overlay"></div>
+
+              <div className="profile-info">
+                <h2>Zoe Miller, 22</h2>
+                <button className="match-btn">+ Mate</button>
+                <p>4 Mutual Mates</p>
+                <div className="tags">
+                  <div className="tag">Bollywood Nights</div>
+                  <div className="tag">Chill Crowd</div>
+                  <div className="tag">Party Regular</div>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div class="actions">
-            <button class="circle-btn">✕</button>
-            <button class="go-btn">🔥 Go Tonight</button>
-            <button class="circle-btn">♡</button>
+            <div className="actions">
+              <button className="circle-btn">✕</button>
+              {/* <button className="go-btn">🔥 Go Tonight</button> */}
+              <Button className="go-btn" variant="primary" onClick={handleShow}>
+                🔥 Go Tonight
+              </Button>
+
+              <button className="circle-btn">♡</button>
+            </div>
           </div>
         </div>
       </div>
+      <div>
+        <Modal className="modal" show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>
+              You choose her{" "}
+              <p className="modal-p">
+                You're about to share her a <span>special invite</span>
+              </p>
+            </Modal.Title>
+          </Modal.Header>
+          <div className="profile-card">
+            <div className="image-box">
+              <img
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1200&auto=format&fit=crop"
+                alt="girl"
+              />
+
+              <div className="overlay"></div>
+
+              <div className="profile-info">
+                <h2>Zoe Miller, 22</h2>
+                <button className="match-btn">+ Mate</button>
+                <p>4 Mutual Mates</p>
+                <div className="tags">
+                  <div className="tag">Bollywood Nights</div>
+                  <div className="tag">Chill Crowd</div>
+                  <div className="tag">Party Regular</div>
+                </div>
+              </div>
+            </div>
+
+            <div className="actions">
+              {/* <button className="circle-btn">✕</button> */}
+              <button className="go-btn">Make your move</button>
+              {/* <button className="circle-btn">♡</button> */}
+            </div>
+            <p className="drink">Add a drink to introduce yourself</p>
+          </div>
+          {/* <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer> */}
+        </Modal>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default MainPage;
--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -21,9 +21,36 @@ const Sidebar = () => (
         </ul>
       </div>
 
-      <div class="bottom-card">
-        <h4>Corporate Employee Offer</h4>
-        <p>1 Month For ₹1</p>
+      <div class="wrapper">
+        <div class="top">
+          <div class="menu-icon">☰</div>
+          <div class="title">More</div>
+        </div>
+
+        <div class="offer-card">
+          <div class="crown">👑</div>
+
+          <div class="offer-text">
+            <h3>Corporate Employee Offer</h3>
+            <p>1 Month For ₹1</p>
+          </div>
+        </div>
+
+        <div class="user-card">
+          <div class="user-left">
+            <img
+              src="https://randomuser.me/api/portraits/men/32.jpg"
+              alt="user"
+            />
+
+            <div class="user-info">
+              <h4>Zeeshan Ahmad</h4>
+              <p>Indus Global Pvt Ltd Admin</p>
+            </div>
+          </div>
+
+          <div class="arrow">↗</div>
+        </div>
       </div>
     </div>
   </>
